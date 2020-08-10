03de44043486261888b61092cd2c7915b55c308e
Abhishek Bagga <abhishekbaggab@gmail.com>
2020-08-10 20:34:18 +0530

    Update ui.R

--- ui.R
+++ ui.R
@@ -7,22 +7,22 @@ TrainSet <- TrainSet[,-1]
 pageWithSidebar(
 
   # Page header
-  headerPanel('Iris Predictor'),
+  headerPanel('Covid Predictor'),
 
   # Input values
   sidebarPanel(
     HTML("<h3>Input parameters</h4>"),
-    sliderInput("Sepal.Length", label = "Sepal Length", value = 5.0,
+    sliderInput("Sepal.Length", label = "Fever", value = 5.0,
                 min = min(TrainSet$Sepal.Length),
                 max = max(TrainSet$Sepal.Length)
     ),
-    sliderInput("Sepal.Width", label = "Sepal Width", value = 3.6,
+    sliderInput("Sepal.Width", label = "Headache", value = 3.6,
                 min = min(TrainSet$Sepal.Width),
                 max = max(TrainSet$Sepal.Width)),
-    sliderInput("Petal.Length", label = "Petal Length", value = 1.4,
+    sliderInput("Petal.Length", label = "Cold", value = 1.4,
                 min = min(TrainSet$Petal.Length),
                 max = max(TrainSet$Petal.Length)),
-    sliderInput("Petal.Width", label = "Petal Width", value = 0.2,
+    sliderInput("Petal.Width", label = "Body Pain", value = 0.2,
                 min = min(TrainSet$Petal.Width),
                 max = max(TrainSet$Petal.Width)),
 
